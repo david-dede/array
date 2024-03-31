6da110db26fe5f3b05213131f2f8ce88641beb15
david-dede <davidenedede@gmail.com>
2024-03-31 22:51:14 +0100

    Update bigDiff.js

--- array1/bigDiff.js
+++ array1/bigDiff.js
@@ -1,5 +1,5 @@
-function bigDiff(nums){
- let min=Math.min(...nums)
- let max=Math.max(...nums) 
- return max-min
-}
+// function bigDiff(nums){
+//  let min=Math.min(...nums)
+//  let max=Math.max(...nums) 
+//  return max-min
+// }
