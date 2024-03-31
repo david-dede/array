febdfc2a1c5c014f37c547cee51de8d7c80ba409
david-dede <davidenedede@gmail.com>
2024-03-31 22:53:13 +0100

    Update biggerTwo.js

--- array1/biggerTwo.js
+++ array1/biggerTwo.js
@@ -1,10 +1,10 @@
-// function biggerTwo(a, b){
-//   let aa=a[0]+a[1]
-//   let bb=b[0]+b[1]
-//   if(aa>=bb){
-//     return [a[0],a[1]]
-//   }
-//     if(aa<bb){
-//     return [b[0],b[1]]
-//   }
-// }
+function biggerTwo(a, b){
+  let aa=a[0]+a[1]
+  let bb=b[0]+b[1]
+  if(aa>=bb){
+    return [a[0],a[1]]
+  }
+    if(aa<bb){
+    return [b[0],b[1]]
+  }
+}
