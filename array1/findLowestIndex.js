eeeecc46a39fdde4aefe8997965491f032a430fc
david-dede <davidenedede@gmail.com>
2024-04-01 23:18:48 +0100

    Update findLowestIndex.js

--- array1/findLowestIndex.js
+++ array1/findLowestIndex.js
@@ -1,10 +1,10 @@
-// function findMinIndex(nums) {
-//     let min = 0;
-//     for (let i = 0; i < nums.length; i++) {
-//         if (nums[i] < nums[min]) {
-//             min = i;
-//         }
-//     }
-//     return min;
-// }
+function findMinIndex(nums) {
+    let min = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < nums[min]) {
+            min = i;
+        }
+    }
+    return min;
+}
 
