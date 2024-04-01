784713d04186f946c62483b089d6beaa528e3d59
david-dede <davidenedede@gmail.com>
2024-04-01 23:18:57 +0100

    Update firstLast6.js

--- array1/firstLast6.js
+++ array1/firstLast6.js
@@ -1,8 +1,8 @@
-function firstLast6(nums){
-  if(nums[0]===6 || nums[nums.length-1]===6){
-    return true
-  }
-  else{
-    return false
-  }
-}
+// function firstLast6(nums){
+//   if(nums[0]===6 || nums[nums.length-1]===6){
+//     return true
+//   }
+//   else{
+//     return false
+//   }
+// }
