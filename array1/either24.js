31a7230c4325a293fa2652bdde5f08697b0c96d9
david-dede <davidenedede@gmail.com>
2024-04-01 23:18:08 +0100

    Update either24.js

--- array1/either24.js
+++ array1/either24.js
@@ -1,29 +1,29 @@
-// // Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.
+// Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.
 
-// // Examples
+// Examples
 
-// // either24([1, 2, 2]) → true
-// // either24([4, 4, 1]) → true
-// // either24([4, 4, 1, 2, 2]) → false
+// either24([1, 2, 2]) → true
+// either24([4, 4, 1]) → true
+// either24([4, 4, 1, 2, 2]) → false
 
 
 
-// function either24(nums) {
-// for(i=0;i<nums.length;i++){
+function either24(nums) {
+for(i=0;i<nums.length;i++){
 
     
     
-//     if (
-//       (nums[i] === 2 && nums[i + 1] === 2) ||
-//       (nums[i] === 4 && nums[i + 1] === 4)
-//     )
-//     {
-//       return true;
-//     }
-//     else{
-//         return false
-//     }
-// }
-// return false
-// }
-// console.log(either24([4, 4, 1, 2, 2]));
+    if (
+      (nums[i] === 2 && nums[i + 1] === 2) ||
+      (nums[i] === 4 && nums[i + 1] === 4)
+    )
+    {
+      return true;
+    }
+    else{
+        return false
+    }
+}
+return false
+}
+console.log(either24([4, 4, 1, 2, 2]));
