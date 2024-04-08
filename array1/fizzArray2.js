b97d75797bbeaee84ecd837f2539e61554d6ca6e
david-dede <davidenedede@gmail.com>
2024-04-08 23:45:47 +0100

    Update fizzArray2.js

--- array1/fizzArray2.js
+++ array1/fizzArray2.js
@@ -1,21 +1,21 @@
-// // Given a number n, create and return a new string array of length n, containing the strings "0", "1" "2" .. through n-1. N may be 0, in which case just return a length 0 array. Note: String(xxx) will make the String form of most types.
+// Given a number n, create and return a new string array of length n, containing the strings "0", "1" "2" .. through n-1. N may be 0, in which case just return a length 0 array. Note: String(xxx) will make the String form of most types.
 
-// // Examples
+// Examples
 
-// // fizzArray2(4) → 0,1,2,3
-// // fizzArray2(10) → 0,1,2,3,4,5,6,7,8,9
-// // fizzArray2(2) → 0,1
-
-
+// fizzArray2(4) → 0,1,2,3
+// fizzArray2(10) → 0,1,2,3,4,5,6,7,8,9
+// fizzArray2(2) → 0,1
 
 
-// function fizzArray2(n) {
-//   let newdata = [];
-//   for (i = 0; i < n; i++) {
-//     newdata.push(i);
-//   }
-//   return newdata;
-// }
 
 
-// console.log(fizzArray2(2));
+function fizzArray2(n) {
+  let newdata = [];
+  for (i = 0; i < n; i++) {
+    newdata.push(i);
+  }
+  return newdata;
+}
+
+
+console.log(fizzArray2(2));
